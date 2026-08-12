c4aaee4758acce4968bde7a2f8f6f61f6730df48
Shubham Salian <shubhamsalian46@gmail.com>
2026-08-12 18:27:59 +0530

    addBestTimeToBuyStockII solution

--- src/arrays/BestTimeToBuyStockII.java
+++ src/arrays/BestTimeToBuyStockII.java
@@ -7,6 +7,7 @@ public class BestTimeToBuyStockII {
         if(prices.length ==0){
             return 0;
         }
+
         int lastPrice = prices[0];
         int totalProfit = 0;
         for(int i=1;i<prices.length;i++){
