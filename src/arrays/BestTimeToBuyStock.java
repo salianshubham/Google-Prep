022907a1c23d161df57fb5a9990d277b006a0800
Shubham Salian <shubhamsalian46@gmail.com>
2026-08-18 16:40:47 +0530

    add empty array exception for BestTimeToBuyStock solution

--- src/arrays/BestTimeToBuyStock.java
+++ src/arrays/BestTimeToBuyStock.java
@@ -20,6 +20,9 @@ public class BestTimeToBuyStock {
 
     // Optimized
     public int optimized(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
         int minPrice = prices[0];
         int maxProfit = 0;
         for(int i=1;i<prices.length;i++){
